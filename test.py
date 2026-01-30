0d7076df43cbcb770321cf0491f84b131ddb7940
Unixprime7 <michaeld801@nycstudents.net>
2026-01-30 09:13:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,19 @@ from turtle import *
 t = Turtle()
 t.speed(100)
 
-sidelength = 100
-rotate = 90
+
 def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
-triangle(100,90)
+
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length + 5
+        t.left(5)
+addSquares(100)
 
 turtle.done()
